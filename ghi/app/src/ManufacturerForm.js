ebb99c86f71b1e010f31b6f01016045100e4fdc0
Nicolette <nmabeza@gmail.com>
2022-10-26 23:37:50 -0700

    updated success alerts

--- ghi/app/src/ManufacturerForm.js
+++ ghi/app/src/ManufacturerForm.js
@@ -27,35 +27,35 @@ const ManufacturerForm = () => {
 
 	return (
 		<div className="row">
-			{!submitted && (
-				<div className="offset-3 col-6">
-					<div className="shadow p-4 mt-4">
-						<h1 className="text-center">Create a Manufacturer</h1>
-						<form id="create-manufacturer-form" onSubmit={handleSubmit}>
-							<div className="form-floating mb-3">
-								<input
-									onChange={(e) => setName(e.target.value)}
-									placeholder="Name"
-									required
-									type="text"
-									name="name"
-									id="name"
-									className="form-control"
-								/>
-								<label htmlFor="name">Manufacturer Name</label>
-							</div>
-							<div className="col text-center">
-								<button className="btn btn-primary">Create</button>
-							</div>
-						</form>
-					</div>
+			<div className="offset-3 col-6">
+				<div className="shadow p-4 mt-4">
+					<h1 className="text-center">Create a Manufacturer</h1>
+					<form id="create-manufacturer-form" onSubmit={handleSubmit}>
+						<div className="form-floating mb-3">
+							<input
+								onChange={(e) => setName(e.target.value)}
+								placeholder="Name"
+								required
+								type="text"
+								name="name"
+								id="name"
+								className="form-control"
+							/>
+							<label htmlFor="name">Manufacturer Name</label>
+						</div>
+						<div className="col text-center">
+							<button className="btn btn-primary">Create</button>
+						</div>
+					</form>
+					{submitted && (
+						<div
+							className="alert alert-success mb-0 p-4 mt-4"
+							id="success-message">
+							Your manufacturer has been created!
+						</div>
+					)}
 				</div>
-			)}
-			{submitted && (
-				<div className="alert alert-success mb-0 p-4 mt-4" id="success-message">
-					Your manufacturer has been created!
-				</div>
-			)}
+			</div>
 		</div>
 	);
 };
--- ghi/app/src/VehicleModelForm.js
+++ ghi/app/src/VehicleModelForm.js
@@ -23,10 +23,9 @@ const VehicleModelForm = () => {
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 
-		const picture_url = pictureUrl
-		const manufacturer_id = selectedManufacturer
+		const picture_url = pictureUrl;
+		const manufacturer_id = selectedManufacturer;
 		const data = { name, picture_url, manufacturer_id };
-
 
 		const modelUrl = "http://localhost:8100/api/models/";
 		const fetchConfig = {
@@ -49,64 +48,64 @@ const VehicleModelForm = () => {
 
 	return (
 		<div className="row">
-			{!submitted && (
-				<div className="offset-3 col-6">
-					<div className="shadow p-4 mt-4">
-						<h1 className="text-center">Create a New Model</h1>
-						<form id="create-model-form" onSubmit={handleSubmit}>
-							<div className="form-floating mb-3">
-								<input
-									onChange={(e) => setName(e.target.value)}
-									placeholder="Model Name"
-									required
-									type="text"
-									name="name"
-									id="name"
-									className="form-control"
-								/>
-								<label htmlFor="name">Model Name</label>
-							</div>
-							<div className="form-floating mb-3">
-								<input
-									onChange={(e) => setPictureUrl(e.target.value)}
-									placeholder="pictureUrl"
-									required
-									type="text"
-									name="pictureUrl"
-									id="pictureUrl"
-									className="form-control"
-								/>
-								<label htmlFor="pictureUrl">Picture Url</label>
-							</div>
-							<div className="mb-3">
-								<select
-									onChange={(e) => setSelectedManufacturer(e.target.value)}
-									required
-									name="manufacturer"
-									id="manufacturer"
-									className="form-select">
-									<option value="">Select a Manufacturer</option>
-									{manufacturers.map((manufacturer) => {
-										return (
-											<option key={manufacturer.id} value={manufacturer.id}>
-												{manufacturer.name}
-											</option>
-										);
-									})}
-								</select>
-							</div>
-							<div className="col text-center">
-								<button className="btn btn-primary">Create</button>
-							</div>
-						</form>
-					</div>
+			<div className="offset-3 col-6">
+				<div className="shadow p-4 mt-4">
+					<h1 className="text-center">Create a New Model</h1>
+					<form id="create-model-form" onSubmit={handleSubmit}>
+						<div className="form-floating mb-3">
+							<input
+								onChange={(e) => setName(e.target.value)}
+								placeholder="Model Name"
+								required
+								type="text"
+								name="name"
+								id="name"
+								className="form-control"
+							/>
+							<label htmlFor="name">Model Name</label>
+						</div>
+						<div className="form-floating mb-3">
+							<input
+								onChange={(e) => setPictureUrl(e.target.value)}
+								placeholder="pictureUrl"
+								required
+								type="text"
+								name="pictureUrl"
+								id="pictureUrl"
+								className="form-control"
+							/>
+							<label htmlFor="pictureUrl">Picture Url</label>
+						</div>
+						<div className="mb-3">
+							<select
+								onChange={(e) => setSelectedManufacturer(e.target.value)}
+								required
+								name="manufacturer"
+								id="manufacturer"
+								className="form-select">
+								<option value="">Select a Manufacturer</option>
+								{manufacturers.map((manufacturer) => {
+									return (
+										<option key={manufacturer.id} value={manufacturer.id}>
+											{manufacturer.name}
+										</option>
+									);
+								})}
+							</select>
+						</div>
+						<div className="col text-center">
+							<button className="btn btn-primary">Create</button>
+						</div>
+					</form>
+					{submitted && (
+						<div
+							className="alert alert-success mb-0 p-4 mt-4"
+							id="success-message">
+							Your model has been created!
+						</div>
+					)}
 				</div>
-			)}
-			{submitted && (
-				<div className="alert alert-success mb-0 p-4 mt-4" id="success-message">
-					Your model has been created!
-				</div>
-			)}
+			</div>
 		</div>
 	);
 };
